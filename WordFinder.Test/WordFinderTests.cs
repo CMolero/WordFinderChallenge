a4f5113c01f4b7c8d709825785a5df28c07303ba
Carlos Molero <moleropaz@gmail.com>
2026-08-19 13:14:32 -0300

    Add project files.

---
+++ WordFinder.Test/WordFinderTests.cs
@@ -0,0 +1,11 @@
+﻿namespace WordFinder.Test
+{
+    public class WordFinderTests
+    {
+        [Fact]
+        public void Placeholder_ProjectCompiles()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ WordFinder/WordFinder.cs
@@ -0,0 +1,14 @@
+﻿namespace WordFinder
+{
+    public class WordFinder
+    {
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
